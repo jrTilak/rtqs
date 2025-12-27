c72aedd84af1999774be5876ab3a12820d55a97d
Suman Kshetri <ksuman20601028@gmail.com>
2025-12-27 14:33:31 +0545

    feat: add quiz feateure added

--- apps/web-client/src/routes/_admin/admin/quizzes/$quiz-id.tsx
+++ apps/web-client/src/routes/_admin/admin/quizzes/$quiz-id.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/_admin/admin/quizes/$quiz-id")({
+export const Route = createFileRoute("/_admin/admin/quizzes/$quiz-id")({
   component: RouteComponent,
 });
 
