cdb2bd2e1dd721716f513a70b3c2f92249437de4
jrtilak <contact@jrtilak.dev>
2026-01-05 20:17:55 +0545

    fix: add auth protection, redirection

--- apps/web-client/src/components/ui/spinner.tsx
+++ apps/web-client/src/components/ui/spinner.tsx
@@ -1,30 +1,38 @@
-import { cn } from "@/lib/utils"
-import { IconLoader } from "@tabler/icons-react"
-import { cva, type VariantProps } from "class-variance-authority"
-import { Slot } from "radix-ui"
-import type { ComponentProps } from "react"
+import { cn } from "@/lib/utils";
+import { IconLoader3 } from "@tabler/icons-react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { Slot } from "radix-ui";
+import type { ComponentProps } from "react";
 
 const loaderVariants = cva("animate-spin", {
   variants: {
     size: {
       none: "",
-      default: "size-4"
-    }
+      default: "size-4",
+      lg: "size-6",
+      xl: "size-8",
+    },
   },
   defaultVariants: {
-    size: "default"
-  }
-})
+    size: "default",
+  },
+});
 
-type SpinnerProps = ComponentProps<"svg"> & VariantProps<typeof loaderVariants> & {
-  asChild?: boolean
-}
+type SpinnerProps = ComponentProps<"svg"> &
+  VariantProps<typeof loaderVariants> & {
+    asChild?: boolean;
+  };
 
 function Spinner({ className, asChild = false, size, ...props }: SpinnerProps) {
-  const Elem = asChild ? Slot.Root : IconLoader
+  const Elem = asChild ? Slot.Root : IconLoader3;
   return (
-    <Elem role="status" aria-label="Loading" className={cn(loaderVariants({ size }), className)} {...props} />
-  )
+    <Elem
+      role="status"
+      aria-label="Loading"
+      className={cn(loaderVariants({ size }), className)}
+      {...props}
+    />
+  );
 }
 
-export { Spinner }
+export { Spinner };
--- apps/web-client/src/routes/index.tsx
+++ apps/web-client/src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { StartLobbyDialog } from "../components/others/user/enter-lobby";
 import {
   Card,
   CardContent,
@@ -8,12 +7,33 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { authClient } from "@/lib/auth-client";
+import { Spinner } from "@/components/ui/spinner";
+import { useEffect } from "react";
+import { ROLE } from "@/server/constants";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const navigate = Route.useNavigate();
+  const { isPending, error, data } = authClient.useSession();
+
+  useEffect(() => {
+    if (isPending) return;
+    if (error) navigate({ to: "/login" });
+    if (data?.user?.role === ROLE.ADMIN) navigate({ to: "/admin" });
+  }, [isPending, data, error]);
+
+  if (isPending) {
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-dvh">
+        <Spinner size={"xl"} className="text-primary" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-muted/40 px-4">
       <Card className="w-full max-w-md shadow-lg">
@@ -26,9 +46,9 @@ function RouteComponent() {
           </CardDescription>
         </CardHeader>
 
-        <CardContent className="flex justify-center">
+        {/* <CardContent className="flex justify-center">
           <StartLobbyDialog />
-        </CardContent>
+        </CardContent> */}
 
         <CardFooter className="justify-center text-xs text-muted-foreground">
           Make sure you have the correct lobby code
